e096d9d94ed79a4606ed2acba8c62306afdc9d65
Eva Lieu <itsevalieu@gmail.com>
2019-05-10 23:42:47 -0700

    Add hero, button for modal

--- client/src/containers/Landing/Landing.jsx
+++ client/src/containers/Landing/Landing.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import SimpleModalWrapped from '../components/Modal';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import hero from './hero.jpg'
 
 const StyledLanding = styled.main`
     height: 100%;
-    border: 1px solid blue;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    background-image: url(${hero});
+    background-size: 100% 100%;
 `;
+const ideas = ['todo', 'cats', 'stuff'];
+class Landing extends React.Component {
+   state = {
+        open: false,
+        selectedValue: ideas[1]
+    };
 
-function Landing() {
-  return (
-    <StyledLanding>
-      Need some inspiration for your next coding project?
-      Well, Let's get pickin'!
-    </StyledLanding>
-  );
+    handleClickOpen = () => {
+        this.setState({
+        open: true,
+        });
+    };
+
+    handleClose = value => {
+        this.setState({ selectedValue: value, open: false });
+    };
+    
+    render() {
+        return (
+            <StyledLanding>
+                <Typography align='center' gutterBottom='true' variant="h3">Codespiration</Typography>
+                <Typography align='center' gutterBottom='true' variant="h5">Need some inspiration for your next coding project?
+                Well, Let's get pickin'!</Typography>
+                <Button variant='contained' color='primary' onClick={this.handleClickOpen}>Get Idea</Button>
+                <SimpleModalWrapped
+                selectedValue={this.state.selectedValue}
+                open={this.state.open}
+                onClose={this.handleClose} />
+            </StyledLanding>
+        );
+    } 
 }
 
 export default Landing;
